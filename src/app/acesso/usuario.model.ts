83be30adc12c89c191679ecdb5802dd6c0e4727d
douglasgmsantos <douglas_motasts@outlook.com>
2018-09-22 22:38:09 -0300

    Criando o modelo USUARIO

---
+++ src/app/acesso/usuario.model.ts
@@ -0,0 +1,9 @@
+export class Usuario{
+    constructor(
+        public email:String,
+        public nome_completo:String,
+        public nome_usuario:String,
+        public senha:String
+    )
+    {}
+}
