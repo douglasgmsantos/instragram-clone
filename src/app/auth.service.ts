d1076d31d7760b01429ef10ddf0923447d0dd1b9
douglasgmsantos <douglas_motasts@outlook.com>
2018-09-23 15:16:16 -0300

    Implementando logout

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -49,4 +49,14 @@ export class auth{
         
         return this.token_id !== undefined        
     }
+
+    public sair():void{
+        firebase.auth().signOut()
+            .then(() =>{
+                localStorage.removeItem("idToken")
+                this.token_id = undefined
+                this.router.navigate(["/"])
+            })
+        
+    }
 }
